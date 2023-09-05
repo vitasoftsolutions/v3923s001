ef41ac0215c60142472b6f896e46a18d06bf26db
iftesamulohy <64037194+iftesamulohy@users.noreply.github.com>
2023-09-05 09:45:40 +0600

    updated video field in who we are

--- vitasoftserver/settings.py
+++ vitasoftserver/settings.py
@@ -137,6 +137,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'upload')
 MEDIA_URL = '/upload/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
